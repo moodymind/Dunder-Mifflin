bdc6c76d4f50a7f8297e284375b245e367d62ab5
Austin Miller <80149004+moodymind@users.noreply.github.com>
2024-06-22 12:33:14 +0200

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import './js/1-header';
+import './js/2-hero';
+import './js/3-about-me';
+import './js/4-tech-skills';
+import './js/5-my-projects';
+import './js/6-benefits';
+import './js/7-faq';
+import './js/8-reviews';
+import './js/8-reviews2';
+import './js/9-work-together';
+import './js/10-footer';
+import './js/11-modal-window';
+import './js/night-mode';
