06bed92a058c3cdbaeb9808f2f3653015e11d0c1
Austin Miller <vadim.sukhetskii@gmail.com>
2024-06-24 15:20:03 +0200

    Added most of the styles and html

--- src/js/2-hero.js
+++
@@ -1,9 +0,0 @@
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-
-document.addEventListener('DOMContentLoaded', () => {
-    AOS.init({
-        reverse: true
-    });
-    AOS.refresh();
-});
--- src/main.js
+++ src/main.js
@@ -1,13 +1,3 @@
-import './js/1-header';
-import './js/2-hero';
-import './js/3-about-me';
-import './js/4-tech-skills';
-import './js/5-my-projects';
-import './js/6-benefits';
-import './js/7-faq';
-import './js/8-reviews';
-import './js/8-reviews2';
-import './js/9-work-together';
-import './js/10-footer';
-import './js/11-modal-window';
-import './js/night-mode';
+import './js/1-hero';
+import './js/2-benefits';
+import './js/3-footer';
